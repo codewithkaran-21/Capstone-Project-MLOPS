0dcc0aee3ca8c46b935bb2c60d335b4647a27de9
Karan Singh <godthatback21@gmail.com>
2025-11-09 17:12:40 +0530

    pushing model from staging to production

---
+++ scripts/promote_model.py
@@ -0,0 +1,41 @@
+import os 
+import mlflow
+
+def promote_model():
+    dagshub_token = os.getenv["CAPSTONE_TEST"]
+    if not dagshub_token:
+        raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+    
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+    dagshub_url = "https://dagshub.com"
+    repo_owner = ""
+    repo_name = ""
+
+    mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+
+    client = mlflow.MlflowClient()
+
+    model_name  = "my_model"
+
+    latest_version_staging = client.get_latest_versions(model_name , stages=["Staging"])[0].version
+
+    prod_version = client.get_latest_versions(model_name , stages=["Production"])
+    for version in prod_version:
+        client.transition_model_version_stage(
+            name=model_name,
+            version=version.version,
+            stage="Archived"
+        )
+
+    client.transition_model_version_stage(
+        name=model_name,
+        version=latest_version_staging,
+        stage="Production"
+    )
+
+    print(f"Model version {latest_version_staging} promoted to Production")
+
+if __name__ == "__main__":
+    promote_model()
